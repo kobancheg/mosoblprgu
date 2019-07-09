172e45c3f4b1592b92a94c01fad514d38f7454ac
kobancheg <aleks.revati@yandex.ru>
2019-07-09 15:33:11 +0300

    header, top menu and added my css file template.css

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
-	<header class="site-header" role="banner">
+	<header class="site-header main-container" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
 			<div class="title-bar-left">
 				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
@@ -32,19 +32,27 @@
 			</div>
 		</div>
 
-		<nav class="site-navigation top-bar" role="navigation" id="<?php foundationpress_mobile_menu_id(); ?>">
+		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</div>
-			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
 
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
+			</div>
+
+            <div class="top-bar-center">
+                <div class="site-desktop-title top-bar-title">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+                </div>
+            </div>
+
+			<div class="top-bar-right">
+
 			</div>
 		</nav>
+        <nav class="top-menu" id="<?php foundationpress_mobile_menu_id(); ?>">
+            <?php foundationpress_top_bar_r(); ?>
+
+            <?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+                <?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+            <?php endif; ?>
+        </nav>
 
 	</header>
