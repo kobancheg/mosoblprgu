e71daded8cb31bf5ec1128f8a75a1b494553d7ee
kobancheg <aleks.revati@yandex.ru>
2019-08-07 10:43:02 +0300

    add home.php

---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+
+/* The template for displaying home page */
+
+get_header(); ?>
+
+    <div class="main-container">
+        <div class="main-grid sidebar-left">
+
+            <main class="main-content">
+                <?php if ( have_posts() ) : query_posts('cat=7&posts_per_page=2'); ?>
+                
+                    <?php /* Start the Loop */ ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+                    <?php endwhile; ?>
+
+                <?php else : ?>
+                    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+                <?php endif; // End have_posts() check.
+                        wp_reset_query();
+                ?>
+
+            </main>
+            <?php get_sidebar(); ?>
+
+        </div>
+    </div>
+
+<?php get_footer();
